011f9b51ddc1bc1a0f78030103c2c807bd0fa308
vikramforsk2019 <vsg16492cse2016@gmail.com>
2020-05-23 22:11:29 +0530

    data analytics using with streamlit

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.title("hare krishna hare krishna krishna krishna \n hare ram hare ram hare ram ram ram hare hare")
+
+# Title
+st.title("AIR Quality Analysis")
+
+# Subheader
+#st.subheader("Simple Data Science App")
+
+# Text
+#st.text("For a simple text")
+
+html_temp = """
+<div style="background-color:tomato;"><p style="color:white;font-size:60px;"> COVID 19 AQI OF INDIA</p></div>
+	"""
+st.markdown(html_temp,unsafe_allow_html=True)
