7be3241f86be7c41d1af73734a8bc178b6a59f48
vikramforsk2019 <vsg16492cse2016@gmail.com>
2020-05-23 22:23:55 +0530

    updaye

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import numpy as np
+import pandas as pd
 # Title
 st.title("AIR Quality Analysis")
 
@@ -13,3 +14,11 @@ html_temp = """
 <div style="background-color:tomato;"><p style="color:white;font-size:60px;"> COVID 19 AQI OF INDIA</p></div>
 	"""
 st.markdown(html_temp,unsafe_allow_html=True)
+df=pd.read_csv('city_day.csv')
+if st.checkbox("Show DataSet"):
+		number = int(st.number_input("Number of Rows to View"))
+		st.dataframe(df.head(number))
+if st.button('column names'):
+ 		st.write(df.columns)
+if st.checkbox('dataset shape'):
+	 st.write(df.shape)
